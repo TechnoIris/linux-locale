775c420877917d879fffc1260fd4735cd0cd5adf
maathmaath <42066081+maathmaath@users.noreply.github.com>
2020-01-22 13:14:48 +0530

    Create set-locale.sh
    it resets your system locale as well as time zone, from the default to desired.

---
+++ set-locale.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "do you need to set a global locale(language only) for *single user* [y/n] ???
+read -p prompt value
+if [$value=='y']
+then
+  locale -a
+  echo "what is your locale"
+  read -p prompt zone
+  cd /.
+  cd root
+  echo "LANG=$zone
+  export LANG">>.bash_profile | .bashrc
+else
+  locale
+  echo "This is your timezone..."
+  locale status
+
+  echo "do you want to set system locale(language)?"
+  read -p "enter [y/n]" option
+  if [$option=='y']
+  then
+    locale -a
+    echo "Type the locale from the list above(with utf coding)."
+    read -p prompt $zone1
+    sudo cd /.
+    sudo cd etc/default/
+    sudo rm -rf locale
+    echo "LANG=$zone1
+    LC_TIME=$zone1>locale
+    sudo update-locale LANG=$zone LANGUAGE
+    locale -k LC_TIME
+fi
